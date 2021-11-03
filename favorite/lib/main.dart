ca70a2fb4a30f6c89676853f65340533cd7127fa
Amanda Shafack <shafack.likhene@gmail.com>
2021-11-03 22:16:03 +0100

    Implement the differen testing techniques (unit, widget and integration tests)

---
+++ favorite/lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:favorite/models/favorites.dart';
+import 'package:favorite/screens/favorites.dart';
+import 'package:favorite/screens/home.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+
+void main() {
+  runApp(TestingApp());
+}
+
+class TestingApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider<Favorites>(
+      create: (context) => Favorites(),
+      child: MaterialApp(
+        title: 'Testing Sample',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        routes: {
+          HomePage.routeName: (context) => HomePage(),
+          FavoritesPage.routeName: (context) => FavoritesPage(),
+        },
+        initialRoute: HomePage.routeName,
+      ),
+    );
+  }
+}
